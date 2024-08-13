931d80d1b36efa57d32a29ac8a4f6471123d4455
Nikhil-Kumar02 <120410585+Nikhil-Kumar02@users.noreply.github.com>
2024-08-13 10:54:13 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function generateRandomNumber(min,max){
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+ document.getElementById("generateButton").addEventListener("click",function(){
+
+    var min = 1;
+    var max = 100;
+
+    var randomNumber = generateRandomNumber(min, max);
+    document.getElementById("result").textContent = randomNumber;
+
+ });
